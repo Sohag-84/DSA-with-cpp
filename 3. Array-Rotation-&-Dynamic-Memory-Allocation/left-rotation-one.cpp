b2b9322728954132cf70baca04e3bc0a93bb676b
Sohag-84 <ihsohag84@gmail.com>
2025-06-27 21:51:05 +0600

    array one time left and right rotation

---
+++ 3. Array-Rotation-&-Dynamic-Memory-Allocation/left-rotation-one.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << endl;
+    cout << "Enter the array elements: ";
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+
+        arr[i - 1] = arr[i];
+    }
+    arr[n - 1] = temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
---
+++ 3. Array-Rotation-&-Dynamic-Memory-Allocation/right-rotation-one.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    cout << endl;
+    cout << "Enter the array elements: ";
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    int temp = arr[n - 1];
+    for (int i = n-2; i >= 0; i--)
+    {
+
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
