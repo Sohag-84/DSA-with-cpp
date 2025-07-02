bc78f5f8dfd09d158f33470467197c1192ef6af1
Sohag-84 <ihsohag84@gmail.com>
2025-07-02 09:45:00 +0600

    letetcode 48 and 54 number problem solved

---
+++ leetcode problem/rotate-image-48.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/rotate-image/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// optimal soluton
+void rotate90Degree(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main()
+{
+    int arr[3][3] = {
+        {1, 2, 3},
+        {3, 4, 5},
+        {6, 7, 8},
+    };
+
+    int ans[3][3];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            ans[j][3 - 1 - i] = arr[i][j];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
