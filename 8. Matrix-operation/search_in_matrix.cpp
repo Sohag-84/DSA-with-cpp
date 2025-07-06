8dee86a9e00f81d074d613b30fd2c4fe1563b573
Sohag-84 <ihsohag84@gmail.com>
2025-07-06 20:43:51 +0600

    search in matrix

---
+++ 8. Matrix-operation/search_in_matrix.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    cout << "Enter row and coloum: ";
+    cin >> row >> col;
+    int arr[row][col];
+    cout << "Enter the array: " << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int input;
+    cout << "Enter the search number: ";
+
+    cin >> input;
+    bool ans = false;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (arr[i][j] == input)
+            {
+                ans = true;
+            }
+        }
+    }
+    if (ans)
+    {
+        cout << "will not take number";
+    }
+    else
+    {
+        cout << "will take number";
+    }
+
+    return 0;
+}
