373a16b0a87265596f4f7e60be2858180f64bb4f
Sohag-84 <ihsohag84@gmail.com>
2025-07-08 18:27:35 +0600

    mirror array

---
+++ 8. Matrix-operation/mirror-array.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int row, col;
+    cout << "Enter row and colum: ";
+
+    cin >> row >> col;
+    int arr[row][col];
+    cout << "Enter the array values: " << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        int first = 0, last = col - 1;
+        while (first < last)
+        {
+            swap(arr[i][first], arr[i][last]);
+            first++;
+            last--;
+        }
+    }
+    cout<<"Final output: "<<endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
