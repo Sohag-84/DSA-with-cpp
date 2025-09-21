f12e5d1bad35bf9bef02230b6f9611dd01033658
Sohag-84 <ihsohag84@gmail.com>
2025-09-21 16:49:43 +0600

    Leetcode 1128 number problem solved

---
+++ 12. Pair and Map/domionos_pair_1128.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/number-of-equivalent-domino-pairs/description/
+
+int numEquivDominoPairs(vector<vector<int>> &dominoes)
+{
+    map<pair<int, int>, int> map;
+
+    for (auto domino : dominoes)
+    {
+        pair<int, int> d = make_pair(domino[0], domino[1]);
+        
+        if (d.first > d.second)
+        {
+            swap(d.first, d.second);
+        }
+        //cout <<"{ " << d.first << "," << d.second <<" }"<< endl;
+        map[d]++;
+    }
+    int ans = 0;
+    for(auto data: map){
+        ans += data.second*(data.second-1)/2;
+    }
+    // for(auto entry:map){
+    //     cout << "(" << entry.first.first << ", "
+    //          << entry.first.second << ") -> "
+    //          << entry.second << endl;
+    // }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> dominoes = {
+        // {1,2},{1,2},{1,1},{1,2},
+        {1,2},{2,1},{3,4},{5,6}
+    };
+    int n = numEquivDominoPairs(dominoes);
+    cout<<"Counter value: "<<n;
+    return 0;
+}
