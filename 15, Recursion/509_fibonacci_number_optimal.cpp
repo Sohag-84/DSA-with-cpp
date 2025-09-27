88fdf6f3e465b50100a78be5d2e74aa42fabfe86
Sohag-84 <ihsohag84@gmail.com>
2025-09-27 12:36:42 +0600

    leetcoe 509 number problem's optimal solution added

--- 15, Recursion/509_fibonacci_number_optimal.cpp
+++ 15, Recursion/509_fibonacci_number_optimal.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// https://leetcode.com/problems/fibonacci-number/description/
 int solve(int n, vector<int> &dp)
 {
     if (n <= 1)
@@ -40,20 +39,9 @@ int fibOptimalSolution(int n)
     return curF;
 }
 
-int fib1(int n)
-{
-    if (n == 0 || n == 1)
-        return n;
-    int result = fib(n - 1) + fib(n - 2);
-    return result;
-}
-
 int main()
 {
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
+    int n = 5;
     cout << fibOptimalSolution(n);
     // cout << fib(n);
-    // cout << fib1(n);
 }
